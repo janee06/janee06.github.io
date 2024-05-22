380aa74c2a50370cccb26febd329ac3f1c35c8b2
janee06 <s2022BockovaJana@skolabaltaci.cz>
2024-05-22 09:41:03 +0200

    my project section

---
+++ script.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});

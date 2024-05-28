aa146bb34334dea89eaa05b290c4ad4fce1547e1
janee06 <s2022BockovaJana@skolabaltaci.cz>
2024-05-28 08:13:25 +0200

    hamburger menu

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
+const links = document.querySelector(".links");
 
 function toggleMenu() {
-  menu.classList.toggle("showMenu");
-  closeIcon.classList.toggle("is-active");
-  menuIcon.classList.toggle("is-active");
+    menu.classList.toggle("showMenu");
+    closeIcon.classList.toggle("is-active");
+    menuIcon.classList.toggle("is-active");
+    links.classList.toggle("showLinks");
 }
 
 hamburger.addEventListener("click", toggleMenu);
-
-$(document).ready(function() {
-  $("#carousel-container .owl-carousel").owlCarousel({
-    items: 3,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 3
-      }
-    }
-  });
-});
